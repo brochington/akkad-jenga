823fc8de041e107b0289c6b2d3c92d2434af8638
Broch Stilley <broch@lumi.com>
2016-05-16 21:25:05 -0700

    Generate block position and rotation.

--- src/scenes/JengaScene/JengaBlocks.js
+++ src/scenes/JengaScene/JengaBlocks.js
@@ -7,32 +7,31 @@ import JengaBlock from './JengaBlock';
 const {Box} = shapes;
 const {Color} = systems;
 
-const pieces = Range(1, 20)
-                    .groupBy(n => n % 3)
+const pieces = Range(0, 12)
+                    .groupBy(n => n % 4)
                     .map((arr, arrIndex) => {
-                        return arrIndex % 2
-                                   ? arr.map((n, i) => [0, (arrIndex * -0.7), i + (i * 0.04)])
-                                   : arr.map((n, i) => [i - 1, (arrIndex * -0.7), i]);
+                        return arr.map((val, i) => {
+                            const position = arrIndex % 2
+                                                 ? [0, 0, i * 1.1]
+                                                 : [i - 1, 1, 1]
+                            const rotation = arrIndex % 2
+                                                 ? [0, 0, 0]
+                                                 : [0, 1.55, 0]
+                            return {
+                                position, rotation
+                            }
+                        })
                     })
                     .toList()
+                    .flatten();
 
 class JengaBlocks extends Component {
     render() {
-        const blocks = pieces
-                            /* Generate blocks */
-                           .map((v, i) => {
-                               return v.map( (f, j) => (
-                                   <JengaBlock
-                                        key={`jenga-block-${i}-${j}`}
-                                        position={f}
-                                        rotation={(i + 1) % 2 ? [0, 0, 0] : [0, 1.57, 0]}
-                                   />
-                               ));
-                           });
+
 
         return (
             <div>
-                {blocks.flatten()}
+                {pieces.map((v, i) => <JengaBlock key={`jenga-block-${i}`} {...v} />)}
             </div>
         );
     }
